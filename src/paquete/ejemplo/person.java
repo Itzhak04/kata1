96d1025c5e9d18f4a9bb24c340f5048151b8034d
itzhak <itzhakprieto@gmail.com>
2023-09-30 17:27:35 +0100

    create class person

---
+++ src/paquete/ejemplo/person.java
@@ -0,0 +1,13 @@
+package paquete.ejemplo;
+
+import java.time.LocalDate;
+
+public class person {
+    final String name;
+    final LocalDate birthday;
+
+    public person(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+}
